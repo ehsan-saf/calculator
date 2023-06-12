b81a8c404996d4dbec30dd22ddecfd58bfc8a26f
M.Ehsan <124778596+ehsan-saf@users.noreply.github.com>
2023-06-12 15:37:17 +0330

    Create a function for choosing the operation

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+let result = 0;
+
+function operate(a, b, operation) {
+    switch(operation) {
+        case "+": result = add(a, b); break;
+        case "-": result = subtract(a, b); break;
+        case "x": result = multiply(a, b); break;
+        case "/": result = divide(a, b); break;
+    }
+} 
